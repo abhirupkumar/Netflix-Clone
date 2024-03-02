55b941958d8de6be2ae210b92f9ba5a3abe09e5f
Abhirup Kumar Bhowmick <94789673+abhirupkumar@users.noreply.github.com>
2024-03-02 10:08:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,12 @@ import type { Metadata } from 'next'
 import App from '@/constants/App';
 
 export const metadata: Metadata = {
+  robots: { index: false, follow: false },
   title: 'NETFLIX',
   description: 'Watch your favorite movies and TV shows on Netflix.',
+  verification: {
+    google: "co0B-CYrqRdI_fmi2ySiu2nYjJd2Wq8Db9ESsAv_uZE"
+  }
 }
 
 export default function RootLayout({
