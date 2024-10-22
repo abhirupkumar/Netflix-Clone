554e6bef90fe1c81197532ab2c358ac20d46cb77
Abhirup Kumar Bhowmick <94789673+abhirupkumar@users.noreply.github.com>
2024-10-22 22:11:32 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import App from '@/constants/App';
 
 export const metadata: Metadata = {
   robots: { index: false, follow: false },
-  title: 'NETFLIX',
+  title: 'NETFLIX CLONE',
   description: 'Watch your favorite movies and TV shows on Netflix.',
   verification: {
     google: "co0B-CYrqRdI_fmi2ySiu2nYjJd2Wq8Db9ESsAv_uZE"
